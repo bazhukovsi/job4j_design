9166f4ff992785b37dc87d7b8fe77af928f9ac72
Сергей <bazhukovsi@yandex.ru>
2022-01-06 02:23:08 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,21 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte variableByte = 127;
+        short variableShort = 500;
+        int variableInt = 1024;
+        long variableLong = 1000000000L;
+        float variableFloat = 1.25F;
+        double variableDouble = 1.53;
+        char variableChar = 64;
+        boolean variableBoolean = true;
+        LOG.debug("Переменная типа byte : variable : {} ", variableByte);
+        LOG.debug("Переменная типа short : variable : {} ", variableShort);
+        LOG.debug("Переменная типа int : variable : {} ", variableInt);
+        LOG.debug("Переменная типа long : variable : {} ", variableLong);
+        LOG.debug("Переменная типа float : variable : {} ", variableFloat);
+        LOG.debug("Переменная типа double : variable : {} ", variableDouble);
+        LOG.debug("Переменная типа char : variable : {} ", variableChar);
+        LOG.debug("Переменная типа boolean : variable : {} ", variableBoolean);
     }
 }
