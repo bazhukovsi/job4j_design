bddefbd5dea5653828820eaed109b8169f103a8d
Сергей <bazhukovsi@yandex.ru>
2022-01-06 11:40:22 +0300

    0. Что такое база данных. [#6860]

---
+++ src/main/java/ru/job4j/script/script.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE car(
+      id serial primary key,
+      name varchar(255),
+	  engine real,
+      description text)
+
+insert into car (name, engine, description) values
+('Seat Altea Freetrack', 2.0, 'Spanish auto');
+
+update car set description = 'Spanish auto full drive';
+
+delete from car;
