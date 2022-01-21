b3632a6f6d9ad783949bef58b1cf319ffd4b3602
bazhukovsi <bazhukovsi@yandex.ru>
2022-01-21 12:40:26 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -4,7 +4,12 @@ import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("c:\\projects\\job4j_design");
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Root folder or extension is null. "
+                    + "Usage java -jar dir.jar ROOT_FOLDER EXTENSION_FILE.");
+        }
+        File file = new File(args[0]);
+        String ext = args[1];
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
