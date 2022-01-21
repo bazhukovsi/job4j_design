34c0491f0803a54dbbfd06a5b99eac163566808a
bazhukovsi <bazhukovsi@yandex.ru>
2022-01-21 15:12:09 +0300

    5.1. Именованные аргументы [#295518]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -7,6 +7,9 @@ public class ArgsName {
     private final Map<String, String> values = new HashMap<>();
 
     public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("Параметр с ключом " + key + " отсутствует.");
+        }
         return values.get(key);
     }
 
