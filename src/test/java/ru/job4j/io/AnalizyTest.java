c40985582f2aa6ab2ea1f2d7b2bd342f85438ba1
Сергей <bazhukovsi@yandex.ru>
2022-01-23 20:05:41 +0300

    3.0. Тестирование IO [#173905]

---
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.io;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class AnalizyTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void serverErrorLogTest() throws IOException {
+        Analizy analizy = new Analizy();
+        File log = folder.newFile("server.log");
+        File unavailable = folder.newFile("unavailable.csv");
+        try (PrintWriter out = new PrintWriter(log)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
+        }
+        analizy.unavailable(log.getAbsolutePath(), unavailable.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(unavailable))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:57:01;10:59:01;11:01:02;11:02:02;"));
+    }
+
+}
