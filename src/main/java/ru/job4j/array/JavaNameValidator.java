7064c9b0ee996477b40f7bfad137ae71ffb86dd7
bazhukovsi <1q2w3e4R!@>
2023-01-02 21:49:03 +0300

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,38 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static void main(String[] args) {
+        isNameValid("first1");
+    }
+
+    public static boolean isNameValid(String name) {
+        if (name.length() == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < name.length(); i++) {
+            int code = name.codePointAt(i);
+            if (i == 0) {
+                if (!isLowerLatinLetter(code)) {
+                    return false;
+                }
+            }
+            if (!(isSpecialSymbol(code) || isUpperLatinLetter(code) || isLowerLatinLetter(code) || Character.isDigit(name.charAt(i)))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 90;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
+}
