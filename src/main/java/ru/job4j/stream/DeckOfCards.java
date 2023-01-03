a119ca200663b1c7cc27e731aa6761f1173601da
bazhukovsi <1q2w3e4R!@>
2023-01-03 14:02:55 +0300

    9. Генерирование колоды карт. FlatMap [#504873]

---
+++ src/main/java/ru/job4j/stream/DeckOfCards.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream;
+
+import java.util.stream.Stream;
+
+public class DeckOfCards {
+    public static void main(String[] args) {
+        Suit[] suits = Suit.values();
+        Value[] values = Value.values();
+        Stream.of(suits)
+                .flatMap(value -> Stream.of(values)
+                        .map(suit -> value + " " + suit))
+                .forEach(System.out::println);
+    }
+}
